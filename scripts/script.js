9f8da0f278bb0e49713e4bbbe4138b103bf40bd5
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-01-14 13:24:20 +0530

    Created project with basic setup

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const toggelSwitch = document.querySelector('input[type="checkbox"]');
+
+// Swich Theme Dynamically
+function switchTheme(event) {}
+
+// Event Listener to listen for change
+toggelSwitch.addEventListener("change", switchTheme);
